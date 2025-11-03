45906500dfe077be52d848ef3028d15819bfe8a3
Aliff Ezaqiel Zaffrulla <aezaqiel@gmail.com>
2025-11-04 01:54:07 +0800

    Independent device queue

--- src/RHI/VulkanDevice.cpp
+++ src/RHI/VulkanDevice.cpp
@@ -8,6 +8,12 @@ namespace PathTracer {
     VulkanDevice::VulkanDevice(std::shared_ptr<VulkanContext> ctx)
         : m_Context(ctx)
     {
+        std::vector<const char*> extensions {
+            VK_KHR_ACCELERATION_STRUCTURE_EXTENSION_NAME,
+            VK_KHR_RAY_TRACING_PIPELINE_EXTENSION_NAME,
+            VK_KHR_DEFERRED_HOST_OPERATIONS_EXTENSION_NAME
+        };
+
         m_RtProps.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_RAY_TRACING_PIPELINE_PROPERTIES_KHR;
         m_RtProps.pNext = nullptr;
 
@@ -17,57 +23,56 @@ namespace PathTracer {
         vkEnumeratePhysicalDevices(m_Context->GetInstance(), &deviceCount, availableDevices.data());
 
         for (const auto& device : availableDevices) {
+            if (!CheckDeviceExtensionSupport(device, extensions)) {
+                continue;
+            }
+
             VkPhysicalDeviceProperties2 props;
             props.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PROPERTIES_2;
             props.pNext = &m_RtProps;
 
             vkGetPhysicalDeviceProperties2(device, &props);
 
-            if (props.properties.deviceType != VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+            if (props.properties.deviceType != VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
                 continue;
+            }
 
             u32 queueCount = 0;
             vkGetPhysicalDeviceQueueFamilyProperties(device, &queueCount, nullptr);
             std::vector<VkQueueFamilyProperties> availableQueues(queueCount);
             vkGetPhysicalDeviceQueueFamilyProperties(device, &queueCount, availableQueues.data());
 
-            std::optional<u32> index;
-            for (u32 i = 0; i < queueCount; ++i) {
-                const auto& queue = availableQueues.at(i);
+            QueueFamilyIndices indices = FindQueueFamilies(device, m_Context->GetSurface());
 
-                if (!(queue.queueFlags & VK_QUEUE_GRAPHICS_BIT)) {
-                    if (queue.queueFlags & VK_QUEUE_COMPUTE_BIT) {
-                        index = i;
-                        break;
-                    }
-                }
-            }
-
-            if (index.has_value()) {
+            if (indices.IsSufficient()) {
                 m_PhysicalDevice = device;
-                m_QueueFamilyIndex = index.value();
+                m_QueueIndices = indices;
 
-                LOG_INFO("Using {}", props.properties.deviceName);
+                LOG_INFO("Physical device: {}", props.properties.deviceName);
+                LOG_INFO("Graphics queue: {}", m_QueueIndices.graphics.value());
+                LOG_INFO("Present queue: {}", m_QueueIndices.present.value());
+                LOG_INFO("Compute queue: {}", m_QueueIndices.compute.value());
+                LOG_INFO("Transfer queue: {}", m_QueueIndices.transfer.value());
+
                 break;
             }
         }
 
         f32 queuePriority[] = { 1.0f };
 
-        VkDeviceQueueCreateInfo queueInfo {
-            .sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
-            .pNext = nullptr,
-            .flags = 0,
-            .queueFamilyIndex = m_QueueFamilyIndex,
-            .queueCount = 1,
-            .pQueuePriorities = queuePriority
-        };
+        std::vector<VkDeviceQueueCreateInfo> queueInfos;
+        queueInfos.reserve(m_QueueIndices.GetUniqueIndices().size());
 
-        std::vector<const char*> extensions {
-            VK_KHR_ACCELERATION_STRUCTURE_EXTENSION_NAME,
-            VK_KHR_RAY_TRACING_PIPELINE_EXTENSION_NAME,
-            VK_KHR_DEFERRED_HOST_OPERATIONS_EXTENSION_NAME
-        };
+        for (const auto& index : m_QueueIndices.GetUniqueIndices()) {
+            queueInfos.push_back(VkDeviceQueueCreateInfo {
+                .sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+                .pNext = nullptr,
+                .flags = 0,
+                .queueFamilyIndex = index,
+                .queueCount = 1,
+                .pQueuePriorities = queuePriority
+            });
+        }
 
         VkPhysicalDeviceBufferDeviceAddressFeatures bufferDeviceAddress {
             .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_BUFFER_DEVICE_ADDRESS_FEATURES,
@@ -119,8 +124,8 @@ namespace PathTracer {
             .sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
             .pNext = &features,
             .flags = 0,
-            .queueCreateInfoCount = 1,
-            .pQueueCreateInfos = &queueInfo,
+            .queueCreateInfoCount = static_cast<u32>(queueInfos.size()),
+            .pQueueCreateInfos = queueInfos.data(),
             .enabledLayerCount = 0,
             .ppEnabledLayerNames = nullptr,
             .enabledExtensionCount = static_cast<u32>(extensions.size()),
@@ -150,13 +155,108 @@ namespace PathTracer {
 
         VK_CHECK(vmaCreateAllocator(&allocatorInfo, &m_Allocator));
 
-        vkGetDeviceQueue(m_Device, m_QueueFamilyIndex, 0, &m_QueueFamily);
+        vkGetDeviceQueue(m_Device, m_QueueIndices.graphics.value(), 0, &m_GraphicsQueue);
+        vkGetDeviceQueue(m_Device, m_QueueIndices.present.value(), 0, &m_PresentQueue);
+        vkGetDeviceQueue(m_Device, m_QueueIndices.compute.value(), 0, &m_ComputeQueue);
+        vkGetDeviceQueue(m_Device, m_QueueIndices.transfer.value(), 0, &m_TransferQueue);
     }
 
     VulkanDevice::~VulkanDevice()
     {
         vmaDestroyAllocator(m_Allocator);
         vkDestroyDevice(m_Device, nullptr);
+    }
+
+    bool VulkanDevice::CheckDeviceExtensionSupport(VkPhysicalDevice device, const std::vector<const char*>& extensions)
+    {
+        u32 extensionCount = 0;
+        vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> availableExtensions(extensionCount);
+        vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, availableExtensions.data());
+
+        std::set<std::string> remainingExtensions(extensions.begin(), extensions.end());
+
+        for (const auto& ext : availableExtensions) {
+            remainingExtensions.erase(ext.extensionName);
+        }
+
+        return remainingExtensions.empty();
+    }
+
+    VulkanDevice::QueueFamilyIndices VulkanDevice::FindQueueFamilies(VkPhysicalDevice device, VkSurfaceKHR surface)
+    {
+        QueueFamilyIndices indices;
+
+        u32 queueCount = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueCount, nullptr);
+        std::vector<VkQueueFamilyProperties> queueFamilies(queueCount);
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueCount, queueFamilies.data());
+
+        std::optional<u32> dedicatedCompute;
+        std::optional<u32> dedicatedTransfer;
+        std::optional<u32> anyCompute;
+        std::optional<u32> anyTransfer;
+
+        u32 index = 0;
+        for (const auto& queue : queueFamilies) {
+            VkBool32 present = VK_FALSE;
+            vkGetPhysicalDeviceSurfaceSupportKHR(device, index, surface, &present);
+            if (present == VK_TRUE) {
+                indices.present = index;
+            }
+
+            if (queue.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+                if (!indices.graphics.has_value()) {
+                    indices.graphics = index;
+                }
+            }
+
+            if (queue.queueFlags & VK_QUEUE_COMPUTE_BIT) {
+                if (!(queue.queueFlags & VK_QUEUE_GRAPHICS_BIT)) {
+                    if (!dedicatedCompute.has_value()) {
+                        dedicatedCompute = index;
+                    }
+                } else {
+                    if (!anyCompute.has_value()) {
+                        anyCompute = index;
+                    }
+                }
+            }
+
+            if (queue.queueFlags & VK_QUEUE_TRANSFER_BIT) {
+                if (!(queue.queueFlags & VK_QUEUE_GRAPHICS_BIT) && !(queue.queueFlags & VK_QUEUE_COMPUTE_BIT)) {
+                    if (!dedicatedTransfer.has_value()) {
+                        dedicatedTransfer = index;
+                    }
+                } else {
+                    if (!anyTransfer.has_value()) {
+                        anyTransfer = index;
+                    }
+                }
+            }
+
+            index++;
+        }
+
+        if (dedicatedCompute.has_value()) {
+            indices.compute = dedicatedCompute;
+        } else if (anyCompute.has_value()) {
+            indices.compute = anyCompute;
+        } else if (indices.graphics.has_value()) {
+            indices.compute = indices.graphics;
+        }
+
+        if (dedicatedTransfer.has_value()) {
+            indices.transfer = dedicatedTransfer;
+        } else if (anyTransfer.has_value()) {
+            indices.transfer = anyTransfer;
+        } else if (indices.compute.has_value() && indices.compute != indices.graphics) {
+            indices.transfer = indices.compute;
+        } else {
+            indices.transfer = indices.graphics;
+        }
+
+        return indices;
     }
 
 }
--- src/RHI/VulkanDevice.hpp
+++ src/RHI/VulkanDevice.hpp
@@ -17,21 +17,62 @@ namespace PathTracer {
         inline VkPhysicalDeviceRayTracingPipelinePropertiesKHR GetRtProps() const { return m_RtProps; }
         inline VkDevice GetDevice() const { return m_Device; }
 
-        inline u32 GetQueueFamilyIndex() const { return m_QueueFamilyIndex; }
-        inline VkQueue GetQueueFamily() const { return m_QueueFamily; }
+        // TODO: Remove
+        inline u32 GetQueueFamilyIndex() const { return m_QueueIndices.compute.value(); }
+        inline VkQueue GetQueueFamily() const { return m_ComputeQueue; }
+
+        inline const VkQueue& GetGraphicsQueue() const { return m_GraphicsQueue; }
+        inline u32 GetGraphicsQueueIndex() const { return m_QueueIndices.graphics.value(); }
+
+        inline const VkQueue& GetPresentQueue() const { return m_PresentQueue; }
+        inline u32 GetPresentQueueIndex() const { return m_QueueIndices.present.value(); }
+
+        inline const VkQueue& GetComputeQueue() const { return m_ComputeQueue; }
+        inline u32 GetComputeQueueIndex() const { return m_QueueIndices.compute.value(); }
+
+        inline const VkQueue& GetTransferQueue() const { return m_TransferQueue; }
+        inline u32 GetTransferQueueIndex() const { return m_QueueIndices.transfer.value(); }
 
         inline VmaAllocator GetAllocator() const { return m_Allocator; }
+
+    private:
+        struct QueueFamilyIndices
+        {
+            std::optional<u32> graphics;
+            std::optional<u32> present;
+            std::optional<u32> compute;
+            std::optional<u32> transfer;
+
+            inline bool IsSufficient() const
+            {
+                return graphics.has_value() && present.has_value();
+            }
+
+            inline std::set<u32> GetUniqueIndices() const
+            {
+                return std::set<u32> { graphics.value(), present.value(), compute.value(), transfer.value() };
+            }
+        };
+
+    private:
+        void PickPhysicalDevice();
+
+        static bool CheckDeviceExtensionSupport(VkPhysicalDevice device, const std::vector<const char*>& extensions);
+        static QueueFamilyIndices FindQueueFamilies(VkPhysicalDevice device, VkSurfaceKHR surface);
 
     private:
         std::shared_ptr<VulkanContext> m_Context;
 
         VkPhysicalDevice m_PhysicalDevice { VK_NULL_HANDLE };
         VkPhysicalDeviceRayTracingPipelinePropertiesKHR m_RtProps;
+        QueueFamilyIndices m_QueueIndices;
 
         VkDevice m_Device { VK_NULL_HANDLE };
 
-        u32 m_QueueFamilyIndex { 0 };
-        VkQueue m_QueueFamily { VK_NULL_HANDLE };
+        VkQueue m_GraphicsQueue;
+        VkQueue m_PresentQueue;
+        VkQueue m_ComputeQueue;
+        VkQueue m_TransferQueue;
 
         VmaAllocator m_Allocator { VK_NULL_HANDLE };
     };
